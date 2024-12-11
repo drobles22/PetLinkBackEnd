4190f965923b3c2f16adf39c49b37bbabc8db370
Diego Andrés Robles Monge <diego.anrm07@gmail.com>
2024-12-10 23:57:43 -0600

    cambiar un metodo y manejo de cor

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
+
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -23,6 +25,8 @@ mongoose
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
+
 
 
 app.get("/",(req,res)=>{
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/post")
+const User = require("../models/user");
+
 
 //create
 
@@ -99,20 +101,33 @@ router.get("/:id", async (req,res)=>{
 
 //post de la feed
 
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
     try {
-      const currentUser = await User.findById(req.body.userId);
+      const currentUser = await User.findById(req.params.userId);
       const userPosts = await Post.find({ userId: currentUser._id });
       const friendPosts = await Promise.all(
         currentUser.followings.map((friendId) => {
           return Post.find({ userId: friendId });
         })
       );
-      res.json(userPosts.concat(...friendPosts))
+      res.status(200).json(userPosts.concat(...friendPosts));
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+  //post del perfil
+
+router.get("/profile/:username", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.params.username });
+      const posts = await Post.find({ userId: user._id });
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 module.exports = router;
