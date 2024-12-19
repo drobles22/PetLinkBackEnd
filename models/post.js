292cf4ee1fabc69a13edbc80ff4352cccf5e2b01
Gabriel Fuentes <68877089+Gafe34@users.noreply.github.com>
2024-12-19 01:40:19 -0600

    add field for post in db

--- models/post.js
+++ models/post.js
@@ -1,47 +1,51 @@
-const mongoose = require("mongoose")
+// models/post.js
 
+const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-
+const PostSchema = new mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required: true,
-      },
-      postdesc: {
-        type: String,
-        max: 500,
-      },
-      attachment: {
-        type: String,
-      },
-      likes: {
-        type: Array,
-        default: [],
-      },
-      shares:{
-        type:Array,
-        default:[]
-      },
-      comentarios: [
-        {
-          userId: {
-            type: String,
-            required: true,
-          },
-          comentario: {
-            type: String,
-            required: true,
-            max: 300,
-          },
-          createdAt: {
-            type: Date,
-            default: Date.now,
-          },
-        },
-      ],
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    postdesc: {
+      type: String,
+      max: 500,
+    },
+    attachment: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    shares: {
+      type: Array,
+      default: [],
+    },
+    comentarios: [
+      {
+        userId: {
+          type: String,
+          required: true,
+        },
+        comentario: {
+          type: String,
+          required: true,
+          max: 300,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    originalAuthor: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
-  );
-  
-  module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
